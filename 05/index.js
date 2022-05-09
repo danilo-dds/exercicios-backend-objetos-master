ce1116d4de7de3ea051d1a6c828e8872e51e0bb8
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-09 19:55:47 -0300

    exercicio 05 feito

---
+++ 05/index.js
@@ -0,0 +1,23 @@
+const curso = {
+    id: 1234,
+    nome: "Lógica de programação",
+    aulas: [{
+        identificador: 01,
+        nomeDaAula: "Introdução a programação"
+    },
+    {
+        identificador: 02,
+        nomeDaAula: "Variáveis"
+    },
+    {
+        identificador: 03,
+        nomeDaAula: "Condicionais"
+    },
+    {
+        identificador: 04,
+        nomeDaAula: "Arrays"
+    },
+    ]
+};
+
+console.log(curso);
