ddb8ced812f180d0a4ef16dacd8e43334d6964cd
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-09 14:59:28 -0300

    exercicio 01 feito

---
+++ 01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Rua da matriz",
+    numero: "156",
+    complemento: "Casa",
+    bairro: "Centro",
+    cep: "45.760.090",
+    cidade: "Nova Lídice",
+    estado: "Bahia",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Danilo",
+    email: "danilo.teste@gmail.com",
+    telefone: "73 3645-2024",
+    data_nascimento: "21/11/1989",
+    endereco
+};
+
+console.log(usuario);
