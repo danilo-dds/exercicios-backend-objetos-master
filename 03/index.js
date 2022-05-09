1f70bb34f81f02d5062eb160358712b80fafefd7
danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-09 18:51:49 -0300

    exercicio 03 feito

---
+++ 03/index.js
@@ -0,0 +1,10 @@
+const usuario = {
+    nome: "Danilo",
+    idade: 32,
+    profissao: "programador",
+    cor_preferida: "preto",
+}
+
+const { nome, idade, profissao, cor_preferida } = usuario;
+console.log(`Olá... Eu sou ${nome} e tenho ${idade} anos de idade.
+Sou ${profissao} e minha cor preferida é ${cor_preferida} !`)
