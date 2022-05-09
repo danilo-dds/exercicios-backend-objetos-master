17c0719687422defd351ef162d6bf8ef37932c2e
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-09 15:18:39 -0300

    exercicio 02 feito

---
+++ 02/index.js
@@ -0,0 +1,28 @@
+const carros = [
+    {
+        marca: "Fiat",
+        modelo: "strada",
+        ano: 2014,
+        cor: "cinza",
+        quantidadePortas: 2,
+        automatico: false
+    },
+    {
+        marca: "volkswagen",
+        modelo: "saveiro",
+        ano: 2020,
+        cor: "vermelho",
+        quantidadePortas: 2,
+        automatico: false
+    },
+    {
+        marca: "toyota",
+        modelo: "corola",
+        ano: 2022,
+        cor: "preto",
+        quantidadePortas: 4,
+        automatico: true
+    }
+];
+
+console.log(carros);
