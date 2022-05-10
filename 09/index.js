480c6ac597b41c901a0cef6f177aad21a5c3b6b0
Danilo Nascimento <danilodds.nascimento@gmail.com>
2022-05-10 18:44:43 -0300

    exercicio 09 feito

---
+++ 09/index.js
@@ -0,0 +1,28 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+];
+let contador = -1;
+for (let i of participantes) {
+    contador++;
+    if (i.nome === "Carlos") {
+        console.log(`Galera... O Carlos está na posição ${contador} , corre lá`)
+        break;
+    }
+}
